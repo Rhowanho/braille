b39c07594c012950db4542f4f6c8150d8a6a3870
Rhowanho <shdhksgh@gmail.com>
2019-09-17 19:11:16 +0900

    Update TTS.py

--- TTS.py
+++ TTS.py
@@ -4,9 +4,9 @@ from gtts import gTTS
 def TTS(String):
     FileName = str(String) + ".mp3"
     tts = gTTS(text=String, lang='ko')
-    tts.save('C:/Users/tjaud/Documents/motor/Flask/Speech/' + FileName)
+    tts.save('./Speech/' + FileName)
 
 def Playing(str):
     mixer.init()
-    mixer.music.load('C:/Users/tjaud/Documents/motor/Flask/Speech/' + str +".mp3")
+    mixer.music.load('./Speech/' + str +".mp3")
     mixer.music.play()
