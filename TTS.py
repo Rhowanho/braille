3fc5f5621e7d75f982887575ed2285a3bbeffe39
Rhowanho <shdhksgh@gmail.com>
2019-09-18 23:55:26 +0900

    Update TTS.py

--- TTS.py
+++ TTS.py
@@ -4,9 +4,9 @@ from gtts import gTTS
 def TTS(String):
     FileName = str(String) + ".mp3"
     tts = gTTS(text=String, lang='ko')
-    tts.save('./Speech/' + FileName)
+    tts.save('/home/ubuntu/braille/Speech/' + FileName)
 
 def Playing(str):
     mixer.init()
-    mixer.music.load('./Speech/' + str +".mp3")
+    mixer.music.load('/home/ubuntu/braille/Speech/' + str +".mp3")
     mixer.music.play()
